a0ac6d58d4fed7ccfd519a99ce6ce2448699cd0b
VARUS636 <Vadim.rusakov.02@mail.ru>
2021-12-15 21:48:27 +0500

    сhange candidate

--- front/candidate-interviev-script.js
+++ front/candidate-interviev-script.js
@@ -1,11 +1,41 @@
+// id данного кандидата
 id_cand = sessionStorage.getItem('id_cand');
 console.log(id_cand);
 
-$("body").on("click", ".set_interviev", function(){
-    console.log(document.getElementById("date_start").value);
-    console.log(document.getElementById("select_vac").value);
+let date_start = document.getElementById("date_start");
+let date_end = document.getElementById("date_end");
+let date_time = document.getElementById("date_time");
+let select_vac = document.getElementById("select_vac");
+
+// добавить в избранное
+$("body").on("click", ".add_to_favorites", function(){
+    if (confirm("Добавить " + "data.s_name" + " в избранное ?")) {
+        // пихнуть данные на сервак
+    }
 });
 
-let dat = "2001-12-10"
-document.getElementById("date_start") = dat;
-console.log(document.getElementById("date_start").value);
+// назначить собеседование
+$("body").on("click", ".set_interview", function(){
+    console.log(date_start.value);
+    console.log(date_end.value);
+    console.log(date_time.value);
+    console.log(select_vac.value);
+
+    if (!date_start.value || !date_end.value || !date_time.value)
+        alert("Заполните все поля");
+    else
+        if (confirm("Подтвердить собеседование?")) {
+           // пихнуть данные на сервак
+        }
+});
+
+function fillInterview(data)
+{
+    //date_start.value = ;
+    //date_end.value = ;
+    //date_time.value = ;
+
+    let option = document.createElement('option');
+    //option.text = ;
+    document.getElementById("select_vac").add(option);
+}
